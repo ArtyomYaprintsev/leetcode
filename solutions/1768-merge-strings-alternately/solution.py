46f31159daf26217be1e8bb16935f613594c1771
ArtyomYaprintsev <yapryntsev.a02@mail.ru>
2024-08-08 01:24:31 +0500

    :white_check_mark: Commit solution

--- solutions/1768-merge-strings-alternately/solution.py
+++ solutions/1768-merge-strings-alternately/solution.py
@@ -1,11 +1,32 @@
-"""Problem: 
+"""Problem: 1768. Merge Strings Alternately.
 
-Description.
+You are given two strings word1 and word2. Merge the strings by adding letters
+in alternating order, starting with word1. If a string is longer than the
+other, append the additional letters onto the end of the merged string.
+
+Return the merged string.
 
 Constraints:
+    1 <= word1.length, word2.length <= 100
+    word1 and word2 consist of lowercase English letters.
 """
 
+from typing import Iterable
 
-# The problem solution
+
 class Solution:
-    pass
+    def convertToGenerator(self, word1: str, word2: str) -> Iterable:
+        len1 = len(word1)
+        len2 = len(word2)
+
+        index = 0
+
+        while index < len1 or index < len2:
+            if index < len1:
+                yield word1[index]
+            if index < len2:
+                yield word2[index]
+            index += 1
+
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        return ''.join(self.convertToGenerator(word1, word2))
--- solutions/1768-merge-strings-alternately/test_solution.py
+++ solutions/1768-merge-strings-alternately/test_solution.py
@@ -9,21 +9,44 @@ def create_solution():
 
 
 @pytest.mark.parametrize(
-    ...,
+    ['word1', 'word2', 'expected'],
+    [
+        pytest.param(
+            '',
+            '',
+            '',
+            id='merge_empty_strings',
+        ),
+        pytest.param(
+            'abc',
+            '',
+            'abc',
+            id='merge_string_with_empty',
+        ),
+        pytest.param(
+            'abc',
+            '123',
+            'a1b2c3',
+            id='merge_strings',
+        ),
+        pytest.param(
+            'abcde',
+            '123',
+            'a1b2c3de',
+            id='merge_strings_with_different_lengths_1',
+        ),
+        pytest.param(
+            'abc',
+            '12345',
+            'a1b2c345',
+            id='merge_strings_with_different_lengths_2',
+        ),
+    ]
 )
-def test_1(solution: Solution, ...):
-    pass
-
-
-@pytest.mark.parametrize(
-    ...,
-)
-def test_2(solution: Solution, ...):
-    pass
-
-
-@pytest.mark.parametrize(
-    ...,
-)
-def test_3(solution: Solution, ...):
-    pass
+def test_merge_strings_alternately(
+    solution: Solution,
+    word1: str,
+    word2: str,
+    expected: str,
+):
+    assert solution.mergeAlternately(word1, word2) == expected
