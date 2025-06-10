4148585acff05ca02c8dbc19dd3a631d4ffa3603
ArtyomYaprintsev <yapryntsev.a02@mail.ru>
2025-06-10 16:59:42 +0500

    🔧 WIP

--- solutions/334-increasing-triplet-subsequence/solution.py
+++ solutions/334-increasing-triplet-subsequence/solution.py
@@ -1,11 +1,51 @@
-"""Problem: 
+"""Problem: 334. Increasing Triplet Subsequence
 
-Description.
+Given an integer array nums, return true if there exists a triple of indices
+(i, j, k) such that i < j < k and nums[i] < nums[j] < nums[k]. If no such
+indices exists, return false.
 
 Constraints:
+    1 <= nums.length <= 5 * 105
+    -2^31 <= nums[i] <= 2^31 - 1
 """
 
 
 # The problem solution
 class Solution:
-    pass
+    def increasingTriplet(self, nums: list[int]) -> bool:
+        if len(nums) < 3:
+            return False
+
+        i_first = 0
+        i_second = 1
+        i_third = 2
+
+        while True:
+            print(i_first, i_second, i_third)
+
+            if nums[i_first] < nums[i_second] < nums[i_third]:
+                return True
+
+            if i_third < len(nums) - 1:
+                i_third += 1
+
+            if i_second < len(nums) - 2 and nums[i_second] >= nums[i_third]:
+                i_second += 1
+
+            if i_first == len(nums) - 3:
+                break
+
+            # if nums[i_first] >= nums[i_second]:
+            i_first += 1
+
+            if i_first == i_second:
+                i_second += 1
+
+        return False
+
+
+if __name__ == "__main__":
+    print(Solution().increasingTriplet([1, 2, 2, 1]))
+    # print(Solution().increasingTriplet([2, 1, 5, 0, 4, 6]))
+    # print(Solution().increasingTriplet([20, 100, 10, 12, 5, 13]))
+    # print(Solution().increasingTriplet([0, 4, 2, 1, 0, -1, -3]))
--- solutions/334-increasing-triplet-subsequence/test_solution.py
+++ solutions/334-increasing-triplet-subsequence/test_solution.py
@@ -9,21 +9,24 @@ def create_solution():
 
 
 @pytest.mark.parametrize(
-    ...,
+    ["input_lst", "expected"],
+    [
+        ([1], False),
+        ([1, 2], False),
+        ([1, 2, 3], True),
+        ([1, 3, 2], False),
+        ([1, 1, 1], False),
+        ([1, 1, 1, 1], False),
+        ([1, 2, 2, 1], False),
+        ([1, 2, 3, 4], True),
+        ([1, 2, 4, 3], True),
+        ([4, 3, 2, 1], False),
+        ([2, 1, 5, 0, 4, 6], True),
+        ([20, 100, 10, 12, 5, 13], True),
+        ([20, 10, 4, 100, 12, 5, 13], True),
+        ([20, 10, 4, 100, 12, 5, 13, 101], True),
+        ([0, 4, 2, 1, 0, -1, -3], False),
+    ],
 )
-def test_1(solution: Solution, ...):
-    pass
-
-
-@pytest.mark.parametrize(
-    ...,
-)
-def test_2(solution: Solution, ...):
-    pass
-
-
-@pytest.mark.parametrize(
-    ...,
-)
-def test_3(solution: Solution, ...):
-    pass
+def test_solution(solution: Solution, input_lst: list[int], expected: bool):
+    assert solution.increasingTriplet(input_lst) == expected
