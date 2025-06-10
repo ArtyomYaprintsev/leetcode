b319d72b55ee030565f3a408b2817be0ca6e1510
ArtyomYaprintsev <yapryntsev.a02@mail.ru>
2025-06-10 13:48:16 +0500

    ✅ Commit solution 238

--- solutions/238-product-of-array-except-self/test_solution.py
+++ solutions/238-product-of-array-except-self/test_solution.py
@@ -9,21 +9,20 @@ def create_solution():
 
 
 @pytest.mark.parametrize(
-    ...,
+    ["input_lst", "expected_lst"],
+    [
+        ([1, 1], [1, 1]),
+        ([1, 2], [2, 1]),
+        ([1, 2, 3], [6, 3, 2]),
+        ([1, 2, 3, 4], [24, 12, 8, 6]),
+        ([-1, 1, 0, -3, 3], [0, 0, 9, 0, 0]),
+        ([-1, 1, 1, -3, 3], [-9, 9, 9, -3, 3]),
+        ([0, 0, 10, 10], [0, 0, 0, 0]),
+    ],
 )
-def test_1(solution: Solution, ...):
-    pass
-
-
-@pytest.mark.parametrize(
-    ...,
-)
-def test_2(solution: Solution, ...):
-    pass
-
-
-@pytest.mark.parametrize(
-    ...,
-)
-def test_3(solution: Solution, ...):
-    pass
+def test_solution(
+    solution: Solution,
+    input_lst: list[int],
+    expected_lst: list[int],
+):
+    assert solution.productExceptSelf(input_lst) == expected_lst
