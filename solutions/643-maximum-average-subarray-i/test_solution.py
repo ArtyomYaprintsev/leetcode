98c8fc9d71a3f9154cda60ea33233b1d75b8d0cf
ArtyomYaprintsev <yapryntsev.a02@mail.ru>
2025-06-18 10:46:25 +0500

    ✅ Commit solution 643

--- solutions/643-maximum-average-subarray-i/test_solution.py
+++ solutions/643-maximum-average-subarray-i/test_solution.py
@@ -9,21 +9,21 @@ def create_solution():
 
 
 @pytest.mark.parametrize(
-    ...,
+    ["input_lst", "input_k", "expected"],
+    [
+        ([1], 1, 1.0),
+        ([1, 2], 2, 1.5),
+        ([-1, 2], 2, 0.5),
+        ([5], 1, 5.00000),
+        ([-1, -2, -3, -4, -5, -100], 2, -1.5),
+        ([-1, -2, -3, -4, -5, 100], 2, 47.5),
+        ([1, 12, -5, -6, 50, 3], 4, 12.75),
+    ],
 )
-def test_1(solution: Solution, ...):
-    pass
-
-
-@pytest.mark.parametrize(
-    ...,
-)
-def test_2(solution: Solution, ...):
-    pass
-
-
-@pytest.mark.parametrize(
-    ...,
-)
-def test_3(solution: Solution, ...):
-    pass
+def test_solution(
+    solution: Solution,
+    input_lst: list[int],
+    input_k: int,
+    expected: float,
+):
+    assert solution.findMaxAverage(input_lst, input_k) == expected
